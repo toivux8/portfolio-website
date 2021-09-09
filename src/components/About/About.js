861101fa63dd9ae63876a0a22fe15fb318643688
Toi Vũ <58621888+toivux8@users.noreply.github.com>
2021-09-09 19:56:57 +0700

    Fix About section

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+
+const About = () => (
+    <Section id="about">
+        <SectionDivider />
+        <SectionTitle>About Me</SectionTitle>
+        <SectionText>I am Toi. I'm a 4th-year student at University of Infomation and Technology. I live in Thu Duc distric, Ho Chi Minh City, Viet Nam. I love to learn new skills and technologies and want to apply my knowledge into building awesome apps. I'm looking forward for opportunities to work in the tech field. Right now I focus more on Front-End technologies like ReactJS. I have strong ability in writing and speaking English, I also have a TOEIC certification with a 905 score</SectionText>
+    </Section>
+);
+
+export default About;
